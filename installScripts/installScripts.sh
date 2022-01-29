2d4983caa4115a5f744920e30b8586a43d2890bd
Dariusz Jankowski <darekjki@gmail.com>
2022-01-29 23:14:58 +0100

    [QA] fix build error; chnage npm to yarn

--- installScripts/installScripts.sh
+++ installScripts/installScripts.sh
@@ -13,9 +13,12 @@ pip3 install -r requirements-dev.txt
 pip3 install -e .
 echo Installing Jupyter labextension ...
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
 cd js
 echo Building AISpace2 frontend ...
 npm run update-lab-extension
 jupyter labextension install
+
+
 cd ..
 jupyter lab
